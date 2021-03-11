3fdf9a55ed16b78a23e88433c0624770c0a3d177
golawojt <golawojt@gmail.com>
2021-03-11 23:38:15 +0100

    some steps...

---
+++ src/MainApp.java
@@ -0,0 +1,50 @@
+public class MainApp {
+    public static void main(String[] args) {
+//        int number = 10;
+//        System.out.println(number);
+//        System.out.println(number);
+//        System.out.println(number);
+//
+//        double doubleNumber = 10.5;
+//        System.out.println(doubleNumber);
+//
+//        String someText = "jakis tekst";
+//        System.out.println(someText);
+//
+//        char someChar = 's';
+//        System.out.println(someChar);
+//
+//        boolean trueValue = true;
+//        System.out.println(trueValue);
+//
+//        boolean falseValue = false;
+//        System.out.println(falseValue);
+//        String homePage = "Home";
+//        String loginPage = "Login";
+//        String contactPage = "Contact";
+//
+//        String page = loginPage;
+//
+//        switch (page) {
+//            case "Home":
+//                System.out.println("jestem na stronie Home");
+//                break;
+//            case "Contact":
+//                System.out.println("jestem na stronie Contact");
+//                break;
+//            case "Login":
+//                System.out.println("jestem na stronie Login");
+//                break;
+//            default:
+//                System.out.println("Nie znam takiej strony");
+//
+//        }
+
+        if (0 % 2 == 0) {
+            System.out.println("liczba parzysta");
+        } else {
+            System.out.println("liczba nieparzysta");
+        }
+
+    }
+}
