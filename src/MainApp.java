277991aa37b970629deed59805a00a3533a46ab4
golawojt <golawojt@gmail.com>
2021-03-18 01:44:55 +0100

    clean code

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,12 +1,5 @@
 public class MainApp {
     public static void main(String[] args) {
-        Bug bug = new Bug("opis błędu", "test@niepodam.pl", 4);
-        bug.showAllInfo();
-        bug.showReporter();
-        bug.showStatus();
-        String bugPriority = bug.getPriority();
-        System.out.println("Priorytet: " + bugPriority);
-
         User wojtek = new User("Wojtek", "Kowalski", "nie@niepodam.pl", 30);
 
         wojtek.getFullName();
