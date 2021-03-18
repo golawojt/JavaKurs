d27ec33bb987246c8768ffc6d751617e2a333863
golawojt <golawojt@gmail.com>
2021-03-18 01:39:32 +0100

    clean code

--- src/MainApp.java
+++ src/MainApp.java
@@ -6,26 +6,5 @@ public class MainApp {
         bug.showStatus();
         String bugPriority = bug.getPriority();
         System.out.println("Priorytet: " + bugPriority);
-
-        User wojtek = new User("Wojtek", "Kowalski", "nie@niepodam.pl", 30);
-
-        wojtek.getFullName();
-        wojtek.getAllInfo();
-        System.out.println(wojtek.getUserAge());
-        int userAge = wojtek.getUserAge();
-        System.out.println(userAge);
-
-        boolean userAdult = wojtek.isUserAdult();
-        System.out.println("Czy Wojtek jest dorosły: " + userAdult);
-
-        wojtek.greetings(wojtek.firstName);
-        wojtek.greetings(wojtek.firstName, wojtek.lastName);
-        wojtek.hoOldAreYou(wojtek.firstName, wojtek.age);
-        wojtek.hoOldAreYou("Asoa", 16);
-
-        System.out.println("In 10 years you'll be " + wojtek.yourAgePlus10(wojtek.age));
-
-        User tomek = new User("tomek", "nowak", "tomek@nowak.pl", 15);
-        tomek.getAllInfo();
     }
 }
