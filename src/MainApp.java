721398bb8a1e44daf33e515932cb9af2308c2665
golawojt <golawojt@gmail.com>
2021-03-18 22:37:09 +0100

    enkapsulacja

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,6 +1,8 @@
+import model.User;
+
 public class MainApp {
     public static void main(String[] args) {
-//        User wojtek = new User("Wojtek", "Kowalski", "nie@niepodam.pl", 30);
+//        model.User wojtek = new model.User("Wojtek", "Kowalski", "nie@niepodam.pl", 30);
 //
 //        wojtek.getFullName();
 //        wojtek.getAllInfo();
@@ -18,42 +20,15 @@ public class MainApp {
 //
 //        System.out.println("In 10 years you'll be " + wojtek.yourAgePlus10(wojtek.age));
 //
-//        User tomek = new User("tomek", "nowak", "tomek@nowak.pl", 15);
+//        model.User tomek = new model.User("tomek", "nowak", "tomek@nowak.pl", 15);
 //        tomek.getAllInfo();
 
-        String str1 = "Hello";
-        String str2 = "hello";
-
-        System.out.println(str1.equals(str2));
-        System.out.println(str1.equalsIgnoreCase(str2));
-
-        String s = str1.toUpperCase();
-        System.out.println(s);
-        s = s.toLowerCase();
-        System.out.println(s);
-
-        System.out.println(str1.startsWith("He"));
-        System.out.println(str1.startsWith("he"));
-        System.out.println(str1.endsWith("lo"));
-        System.out.println(str1.endsWith("lo!"));
-        System.out.println(str1.contains("ll"));
-
-        System.out.println(" ".isEmpty());
-
-        String replaceStr = str1.replace("o", "");
-        System.out.println(replaceStr);
-
-        String replaceAll = str1.replaceAll("l", "X");
-        System.out.println(replaceAll);
-
-        String substring = str1.substring(1, 4);
-        System.out.println(substring);
-
-        String textWithWhitespaces = "    asd   soi aaaaa       ";
-        System.out.println(textWithWhitespaces.length());
-
-        String trim = textWithWhitespaces.trim();
-        System.out.println(trim);
-        System.out.println(trim.length());
+        User user = new User("Wojtek", "Kowalski", "nie@niepodam.pl", 20);
+        System.out.println(user.isUserAdult());
+        System.out.println(user.getEmail());
+        user.setEmail("takniepodam.pl");
+        user.setEmail("tak@niepodam.ru");
+        user.setEmail("tak@niepodam.RU");
+        System.out.println(user.getEmail());
     }
 }
---
+++ src/model/User.java
@@ -0,0 +1,93 @@
+package model;
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private int age;
+    private boolean isAdult;
+
+    public User(String firstName, String lastName, String email, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.age = age;
+        this.isAdult = isUserAdult();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        if (email.contains("@") && !email.endsWith(".ru")) {
+            this.email = email;
+        } else {
+            System.out.println("Wrong email format");
+        }
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isAdult() {
+        return isAdult;
+    }
+
+    public void setAdult(boolean adult) {
+        isAdult = adult;
+    }
+
+    public void getFullName() {
+        System.out.println(firstName + " " + lastName);
+    }
+
+    public void getAllInfo() {
+        System.out.println(firstName + " " + lastName + " " + email + " " + age + " " + isAdult);
+    }
+
+    public int getUserAge() {
+        return age;
+    }
+
+    public boolean isUserAdult() {
+        return age >= 18;
+    }
+
+    public void greetings(String name) {
+        System.out.println("Hi " + name + ". Nice to see you!");
+    }
+
+    public void greetings(String name, String lastName) {
+        System.out.println("Hi " + name + " " + lastName + ". Nice to see you!");
+    }
+
+    public void hoOldAreYou(String name, int age) {
+        System.out.println("Hi " + name + ". You're " + age + " old.");
+    }
+
+    public int yourAgePlus10(int age) {
+        return age + 10;
+    }
+}
